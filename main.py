c4150fca97fc8529c7b231192057e39f132eafa8
Valter Junior <vjunior@featcode.com.br>
2024-03-02 17:13:32 -0300

    Base project added.

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+from decouple import config
+import psycopg2
+import pandas as pd
+import gspread
+from gspread_dataframe import set_with_dataframe
+
+from infrastructure.data import model
+from infrastructure.data.database import engine
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+model.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
