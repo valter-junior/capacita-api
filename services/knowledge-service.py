2a29e34b0690eb96e7029044ae8be9d5f263ccee
Raul Coelho <rpc3@cin.ufpe.br>
2024-03-02 19:21:54 -0300

    controller service and repository added with adjustments

--- services/knowledge-service.py
+++ services/knowledge-service.py
@@ -1,11 +1,16 @@
+from infrastructure.data.database import get_db
+from fastapi import Depends, Query
+from infrastructure.repositores.knowledge_repository import KnowledgeRepository
+
+
 class KnowledgeService:
-    def __init__(self, knowledge_repo):
-        self.knowledge_repo = knowledge_repo
+    def __init__(self, knowledge: KnowledgeRepository):
+        self.knowledge = knowledge
 
     def get_knowledge(self, knowledge_id):
-        return self.knowledge_repo.get(knowledge_id)
+        return self.knowledge.get(knowledge_id)
 
     def create_knowledge(self, knowledge):
-        return self.knowledge_repo.create(knowledge)
+        return self.knowledge.create_knowledgement(knowledge)
 
  
